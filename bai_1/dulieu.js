242b0215ee8bec29916a7ea4849d1ce4a87dbf72
nghia.nguyen <nghia.nguyen20220@gmail.com>
2021-01-28 09:49:45 +0700

    commit nodejs

---
+++ bai_1/dulieu.js
@@ -0,0 +1,23 @@
+dulieu = {
+    "danhsach": [{
+            "masv": 1,
+            "tensv": "abc",
+            "gmail": "ad@itc.edu.vn"
+        },
+        {
+            "masv": 2,
+            "tensv": "Trọng Hưng",
+            "gmail": "trHung@itc.edu.vn"
+        },
+        {
+            "masv": 3,
+            "tensv": "Pyy xinh đẹp",
+            "gmail": "Pyy@itc.edu.vn"
+        },
+        {
+            "masv": 4,
+            "tensv": "Ngân ngáo",
+            "gmail": "Ngan@itc.edu.vn"
+        }
+    ]
+}
